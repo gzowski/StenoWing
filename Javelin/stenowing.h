8ae984b9c7b019dbab1afd1531f6487f6ebf2695
James Gzowski <flexyjerkov@gmail.com>
2025-04-04 12:14:33 +0100

    StenoWing V2

--- Javelin/stenowing.h
+++ Javelin/stenowing.h
@@ -1,20 +1,17 @@
- //---------------------------------------------------------------------------
-
 #pragma once
 #include "main_flash_layout.h"
-
-//---------------------------------------------------------------------------
 
 #define JAVELIN_USE_EMBEDDED_STENO 1
 #define JAVELIN_USE_USER_DICTIONARY 1
 #define JAVELIN_USB_MILLIAMPS 100
+
 #define JAVELIN_DEBOUNCE_MS 10
 #define JAVELIN_BUTTON_MATRIX 1
 
 constexpr uint8_t COLUMN_PINS[] = {2, 3, 4, 5, 6, 20, 22, 26, 27, 28, 29};
 constexpr uint8_t ROW_PINS[] = {7, 23, 8};
-constexpr uint32_t COLUMN_PIN_MASK = 0x1b8007e;
-constexpr uint32_t ROW_PIN_MASK = 0x2060000;
+constexpr uint32_t COLUMN_PIN_MASK = 0x3E50007C;
+constexpr uint32_t ROW_PIN_MASK = 0x00800180;
 
 constexpr int8_t KEY_MAP[3][16] = {
   {  0,  1,  2,  3,  4, /**/  5,  6,  7,  8,  9, 10 },
@@ -29,9 +26,20 @@ constexpr int8_t KEY_MAP[3][16] = {
 
 constexpr uint8_t RGB_MAP[JAVELIN_RGB_COUNT] = {
    0, 1, 2, 3, 4, 5, /**/ 6, 7, 8, 9, 10,
-  11,12,13,14,16,17, /**/ 18,19,20,21,22,
-           23,24,25, /**/ 26,27,28
+  11,12,13,14,15,16, /**/ 17,18,19,20,21,
+           22,23,24, /**/ 25,26,27
 };
+
+#define JAVELIN_KEYS( \
+    S1, TL, PL, HL, ST1, ST3, FR, PR, LR, TR, DR, \
+    S2, KL, WL, RL, ST2, ST4, RR, BR, GR, SR, ZR, \
+    N1, A, O, E, U, N2 \
+) \
+{ \
+    { S1, TL, PL, HL, ST1, ST3, FR, PR, LR, TR, DR }, \
+    { S2, KL, WL, RL, ST2, ST4, RR, BR, GR, SR, ZR }, \
+    { N1, A, O, E, U, N2 }  \
+}
 
 #define JAVELIN_SCRIPT_CONFIGURATION \
   R"({"name":"StenoWing","layout":[{"x":0,"y":0},{"x":1,"y":0},{"x":2,"y":0},{"x":3,"y":0},{"x":4,"y":0},{"x":5,"y":0},{"x":6,"y":0},{"x":7,"y":0},{"x":8,"y":0},{"x":9,"y":0},{"x":10,"y":0},{"x":0,"y":1},{"x":1,"y":1},{"x":2,"y":1},{"x":3,"y":1},{"x":4,"y":1},{"x":5,"y":1},{"x":6,"y":1},{"x":7,"y":1},{"x":8,"y":1},{"x":9,"y":1},{"x":10,"y":1},{"x":2,"y":2},{"x":3,"y":2},{"x":4,"y":2},{"x":5,"y":2},{"x":6,"y":2},{"x":7,"y":2}]})"
@@ -39,4 +47,5 @@ constexpr uint8_t RGB_MAP[JAVELIN_RGB_COUNT] = {
 const size_t BUTTON_COUNT = 28;
 const char *const MANUFACTURER_NAME = "gzowski";
 const char *const PRODUCT_NAME = "StenoWing (Javelin)";
-const int VENDOR_ID = 0x39DF;//---------------------------------------------------------------------------
+const int VENDOR_ID = 0x31C8;
+
--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# StenoWing
+# StenoWing28 - Rev 2
 28 key staggered Stenography keyboard without number row
+![stenowing28](https://github.com/gzowski/StenoWing/blob/main/images/stenowing_light.jpg)
 
 ## Features
 * 28 Key layout
 * Per key RGB LED's
+* Javelin firmware (Reqires RP2040 pro micro with 16MB storage)
+* Vertical stagger
+* Double tap #2 to change mode and configure RGB
+
+## Parts
+
+| Part | Quantity     | Description                |
+| :-------- | :------- | :------------------------- |
+| Diodes| 28  | Through-hole 1N4148 |
+| Microcontroller | 1 | Pro Micro RP2040 with 16MB Storage |
+| Switches | 28 | MX or Choc V1 (67 instead of rotary encoder) |
+| Keycaps  | 28 | 1u keycaps |
+
+## Optional Parts
+
+| Part | Quantity     | Description                |
+| :-------- | :------- | :------------------------- |
+| LED Diodes | 28 |  SK6812 MINI-E |
+| Base Plate | 1 | STEP file in 3DPrint folder |
+| M2 Standoff | 6 | M2x8mm |
+| M2 Screws | 12 | M2x6mm |
+
+##Build Guide
+
+1. Solder the Pro Micro controller to the PCB on the top side, with components facing up
+2. Solder the diodes onto the top side of the board, observe orientation.
+  - Left side (Black line down), Middle (Black line up), Right (Black line down)
+  - Snip off the excess legs from the diodes from the underside once all soldered
+3. If using LED's you'll need to place these on the rear side of the PCB, observe the triangle marking on the PCB, this corrosponds to the one leg with an edge cut into it.
+  - The LED's are orientated differently on the lowest row due to rotated switches.
+4. Insert all switches into the PCB, turn over and solder each switch.
+  - Ensure that the switch is seated into the PCB fully before soldering.
+5. Place keycaps on top
+6. Install desired firmware by putting the controller into BOOT mode.
